0e3ebb296bf569ccf56cff6b1b729c2fd6311a8b
octor0k <luis.castellanos2942@alumnos.udg.mx>
2022-04-29 21:46:58 -0500

    holaaaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,6 +38,7 @@
 
             {{-- HOLA MUNDO --}}
             {{-- HOLA MUNDO --}}
+            {{-- HOLA MUNDO --}}
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
