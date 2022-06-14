2c1c51a744463687a99b77a73cb1b1c5c461c6ce
Esteban Sevilla <esteban@perspective.com.mx>
2022-06-13 21:49:35 -0500

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white w-full  px-4 py-2">
-    <div class="flex flex-col-reverse md:flex-row items-center">
+    <div class="flex flex-col-reverse md:flex-row md:items-center">
         <div class="ml-6 md:ml-0 md:w-9/12">
             <h5>CONTACTANOS</h5>
             <nav>
