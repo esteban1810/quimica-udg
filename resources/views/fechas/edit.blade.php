a196871b92d0bed4133a1f4d8054b36f3dbdc03a
Luis Aguilar <lmartin.aguilar@alumnos.udg.mx>
2022-07-11 19:45:44 -0500

    Fechas-estilos

--- resources/views/fechas/edit.blade.php
+++ resources/views/fechas/edit.blade.php
@@ -1,7 +1,13 @@
-edicion de fechas 
+@extends('layouts.app')
 
+@section('content')
+
+<div class=" w-full bg-background py-8">
 <form action="{{url('/fechas/'.$fechas->id)}}" method="post">
 @csrf
 {{ method_field('PATCH') }}
 @include('fechas.form')
 </form>
+</div>
+
+@endsection
